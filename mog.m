060ca8f8304ce4658c4f464dceda00b5345ffd08
carlosgalvezp <carlosgalvezp@gmail.com>
2013-12-18 12:00:53 +0100

    Create MoG function

---
+++ mog.m
@@ -0,0 +1,10 @@
+function [ output_args ] = mog(image)
+%MOG Summary of this function goes here
+%   Detailed explanation goes here
+
+ALPHA = 0.1; %Learning rate
+M = 3;       %Number of Gaussians
+SIGMA0=0.01; %Initial variance
+T = 0.5      %Background Threshold
+end
+
