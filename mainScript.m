b267d3eeb504f6afc40399c78bc463c07d07e366
carlosgalvezp <carlosgalvezp@gmail.com>
2014-01-13 17:47:22 +0100

    edited README

--- mainScript.m
+++ mainScript.m
@@ -5,11 +5,13 @@ clear all
 load('myBreakpoints.mat');
 dbstop(s);
 
-
-videoFile = 'viptraffic.mp4';
+%Configuration
+videoFile = 'viptraffic.mp4'; %Input video file
 filter = 0; %KF = 0, PF = 1
-writeToFile = 0;
-filename = 'visionKF';
-verbose=0;
-RESIZE = 1;%1 = No resizing, 0.5 = image resized to a half
+writeToFile = 0; %0 = no, 1 = yes
+filename = 'visionKF'; %name of the output video file
+verbose=0; %1 = display particles
+RESIZE = 1; %1 = No resizing, 0.5 = image resized to a half, etc
+
+%Execution
 main(videoFile,filter,RESIZE,writeToFile,filename,verbose);
