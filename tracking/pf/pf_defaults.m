74414ab634550a6162d04bd3faa035b55adb67c1
Michal Staniaszek <m.staniaszek@gmail.com>
2014-01-13 17:53:53 +0100

    PF set to 3000 particles

--- tracking/pf/pf_defaults.m
+++ tracking/pf/pf_defaults.m
@@ -6,7 +6,7 @@ function [parameters] = pf_defaults()
 process_noise = diag([10 10 5 5 5 5]);
 % noise on x and y measurements
 measurement_noise = diag([5 5 5 5]);
-nparticles = 1000;
+nparticles = 3000;
 
 parameters = struct('process_noise',process_noise,'measurement_noise',measurement_noise,'nparticles',nparticles);
 
